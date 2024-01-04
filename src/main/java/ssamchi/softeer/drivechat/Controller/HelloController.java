77d030bcd671ae3f29556c1cddb220e6e2fa2596
timido7021 <timido7021@gmail.com>
2024-01-04 14:47:49 +0900

    Feat : RestController 추가함.

---
+++ src/main/java/ssamchi/softeer/drivechat/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package ssamchi.softeer.drivechat.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello";
+    }
+}
