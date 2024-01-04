ce655eef99aaba1085d81754e9f22fe196f154a5
timido7021 <timido7021@gmail.com>
2024-01-04 13:46:39 +0900

    initial commit

---
+++ src/main/java/ssamchi/softeer/drivechat/DrivechatApplication.java
@@ -0,0 +1,13 @@
+package ssamchi.softeer.drivechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivechatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssamchi/softeer/drivechat/DrivechatApplicationTests.java
@@ -0,0 +1,13 @@
+package ssamchi.softeer.drivechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
