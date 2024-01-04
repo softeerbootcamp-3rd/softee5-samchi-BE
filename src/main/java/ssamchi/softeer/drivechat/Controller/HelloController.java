34921b1f10a7cd5efa821cc4f9fc26da1ca418c1
99winnmin <ojysep9987@gmail.com>
2024-01-04 17:11:04 +0900

    :green_heart: Fix : CI 파일 수정6

--- src/main/java/ssamchi/softeer/drivechat/Controller/HelloController.java
+++ src/main/java/ssamchi/softeer/drivechat/Controller/HelloController.java
@@ -1,5 +1,6 @@
 package ssamchi.softeer.drivechat.Controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +11,10 @@ public class HelloController {
     @GetMapping("hello")
     public String hello() {
         return "Hello";
+    }
+
+    @GetMapping("health/check")
+    public ResponseEntity<String> healthChecking() {
+        return ResponseEntity.ok("OK");
     }
 }
