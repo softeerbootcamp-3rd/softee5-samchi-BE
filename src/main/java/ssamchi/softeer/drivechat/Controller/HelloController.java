49e470159f7aaae50e8da218209dc27efd54b9af
99winnmin <ojysep9987@gmail.com>
2024-01-04 15:18:23 +0900

    :poop: Chore : Health check url 수정

--- src/main/java/ssamchi/softeer/drivechat/Controller/HelloController.java
+++ src/main/java/ssamchi/softeer/drivechat/Controller/HelloController.java
@@ -1,12 +1,13 @@
 package ssamchi.softeer.drivechat.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/")
 public class HelloController {
-    @RequestMapping("/")
+    @GetMapping("hello")
     public String hello() {
         return "Hello";
     }
