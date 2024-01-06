b3487c5f84c16b280bf92a1e941acd5ed91c814d
99winnmin <ojysep9987@gmail.com>
2024-01-06 11:10:49 +0900

    :sparkles: Fix : ERD 변경으로 인한 도메인 클래스 추가

---
+++ src/main/java/ssamchi/softeer/drivechat/domain/DriverTopic.java
@@ -0,0 +1,36 @@
+package ssamchi.softeer.drivechat.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DriverTopic extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long driverId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "driver_id")
+    private Driver driver;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "topic_id")
+    private Topic topic;
+
+    @Builder
+    public DriverTopic(Driver driver, Topic topic){
+        this.driver = driver;
+        this.topic = topic;
+    }
+}
