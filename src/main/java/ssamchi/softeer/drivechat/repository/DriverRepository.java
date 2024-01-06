4d7206a28a590f0834ddff01499c93bc512167a4
99winnmin <ojysep9987@gmail.com>
2024-01-06 17:14:36 +0900

    :beers: Feat : Driver 마커 전체 조회 JPA 추가 및 스프링부트 JPA Auditing 활성화

--- src/main/java/ssamchi/softeer/drivechat/repository/DriverRepository.java
+++ src/main/java/ssamchi/softeer/drivechat/repository/DriverRepository.java
@@ -1,8 +1,10 @@
 package ssamchi.softeer.drivechat.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ssamchi.softeer.drivechat.domain.Driver;
 
 public interface DriverRepository extends JpaRepository<Driver, Long> {
     Long countByUser_UserIdAndFoundIsTrue(Long userId);
+    List<Driver> findDriversByFoundIsFalse();
 }
--- src/main/java/ssamchi/softeer/drivechat/repository/DriverTopicRepository.java
+++ src/main/java/ssamchi/softeer/drivechat/repository/DriverTopicRepository.java
@@ -2,9 +2,17 @@ package ssamchi.softeer.drivechat.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ssamchi.softeer.drivechat.domain.DriverTopic;
 
 public interface DriverTopicRepository extends JpaRepository<DriverTopic, Long> {
-    List<DriverTopic> findAllByDriverTopicId(Long driverId);
+    List<DriverTopic> findAllByDriver_DriverId(Long driverId);
+
+    @Query("SELECT DISTINCT dt.driver.driverId "
+        + "FROM DriverTopic dt INNER JOIN GuestTopic gt ON dt.topic.topicId = gt.topic.topicId "
+        + "WHERE gt.guest.guestId = :guestId and dt.driver.found = false "
+        + "ORDER BY dt.driver.createdAt DESC ")
+    List<Long> findSameTopicDriverIdsByGuestId(@Param("guestId") Long guestId);
 
 }
