358bf57dd4ce7a92e5a5e5d9fe9df93e4e68d68e
99winnmin <ojysep9987@gmail.com>
2024-01-06 17:13:39 +0900

    :sparkles: Feat : 동승자 정보 not found error 추가

--- src/main/java/ssamchi/softeer/drivechat/exception/Error.java
+++ src/main/java/ssamchi/softeer/drivechat/exception/Error.java
@@ -7,7 +7,8 @@ public enum Error {
     INTERNAL_SERVER_ERROR("서버 내부 에러입니다.", 500),
     // 사용자
     USER_NOT_FOUND("사용자를 찾을 수 없습니다.", 1000),
-    MARKER_NOT_FOUND("운전자를 찾을 수 없습니다.", 1001);
+    MARKER_NOT_FOUND("운전자를 찾을 수 없습니다.", 1001),
+    GUEST_NOT_FOUND("동승자 정보를 찾을 수 없습니다.", 1002);
 
     private final String message;
     private final int errorCode;
