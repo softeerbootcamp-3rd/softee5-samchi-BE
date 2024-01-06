7922a89fa38489ab430d9cbb2836b4e350f595f7
99winnmin <ojysep9987@gmail.com>
2024-01-06 17:14:56 +0900

    :sparkles: Feat : Driver 마커 전체 조회 비지니스 로직 추가

--- src/main/java/ssamchi/softeer/drivechat/service/MarkerService.java
+++ src/main/java/ssamchi/softeer/drivechat/service/MarkerService.java
@@ -2,18 +2,22 @@ package ssamchi.softeer.drivechat.service;
 
 import jakarta.transaction.Transactional;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ssamchi.softeer.drivechat.domain.Driver;
 import ssamchi.softeer.drivechat.domain.DriverTopic;
+import ssamchi.softeer.drivechat.domain.Guest;
 import ssamchi.softeer.drivechat.domain.User;
 import ssamchi.softeer.drivechat.dto.request.RequestDriverMarkerMakeDto;
 import ssamchi.softeer.drivechat.dto.response.DriverMarkerDetailInfoResponseDto;
+import ssamchi.softeer.drivechat.dto.response.MainMapMarkerIdListResponseDto;
 import ssamchi.softeer.drivechat.dto.response.ResponseDriverMarkerMakeDto;
 import ssamchi.softeer.drivechat.exception.BusinessException;
 import ssamchi.softeer.drivechat.exception.Error;
 import ssamchi.softeer.drivechat.repository.DriverRepository;
 import ssamchi.softeer.drivechat.repository.DriverTopicRepository;
+import ssamchi.softeer.drivechat.repository.GuestRepository;
 import ssamchi.softeer.drivechat.repository.UserRepository;
 import ssamchi.softeer.drivechat.utils.HeaderUtils;
 
@@ -23,6 +27,7 @@ public class MarkerService {
     private final UserRepository userRepository;
     private final DriverRepository driverRepository;
     private final DriverTopicRepository driverTopicRepository;
+    private final GuestRepository guestRepository;
 
     @Transactional
     public ResponseDriverMarkerMakeDto registrationDriverMarker(RequestDriverMarkerMakeDto requestDriverMarkerMakeDto) {
@@ -56,13 +61,13 @@ public class MarkerService {
             .orElseThrow(() -> BusinessException.of(Error.MARKER_NOT_FOUND));
 
         Long driveChatCount = driverRepository.countByUser_UserIdAndFoundIsTrue(driver.getUser().getUserId());
-        List<DriverTopic> driverTopics = driverTopicRepository.findAllByDriverTopicId(
+        List<DriverTopic> driverTopics = driverTopicRepository.findAllByDriver_DriverId(
             driver.getDriverId());
 
         List<String> topics = driverTopics.stream()
-            .map(driverTopic -> driverTopic.getTopic().getContentsList())
-            .toList()
-            .stream().flatMap(List::stream).toList();
+            .flatMap(driverTopic -> driverTopic.getTopic().getContentsList().stream())
+            .collect(Collectors.toList());
+
 
         return DriverMarkerDetailInfoResponseDto.builder()
             .driverId(driver.getDriverId())
@@ -72,5 +77,25 @@ public class MarkerService {
             .startTime(driver.getEstimatedTime())
             .driverTopics(topics)
             .build();
+    }
+
+    public MainMapMarkerIdListResponseDto searchDriverMarker(Boolean sort) {
+        String nickname = HeaderUtils.getHeader("nickname");
+        User user = userRepository.findByUserName(nickname)
+            .orElseThrow(() -> BusinessException.of(Error.USER_NOT_FOUND));
+
+        Guest guest = guestRepository.findByUser_UserId(user.getUserId())
+            .orElseThrow(() -> BusinessException.of(Error.GUEST_NOT_FOUND));
+        if (sort) {
+            List<Long> sameTopicDriverIds = driverTopicRepository.findSameTopicDriverIdsByGuestId(guest.getGuestId());
+            return MainMapMarkerIdListResponseDto.builder()
+                .markerIdList(sameTopicDriverIds)
+                .build();
+        } else {
+            List<Long> allDriverIds = driverRepository.findDriversByFoundIsFalse().stream().map(Driver::getDriverId).toList();
+            return MainMapMarkerIdListResponseDto.builder()
+                .markerIdList(allDriverIds)
+                .build();
+        }
     }
 }
