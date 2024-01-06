e98c901fac586aab092f6ba42fb818aaca441d57
99winnmin <ojysep9987@gmail.com>
2024-01-06 17:15:34 +0900

    :beers: Feat : Guest Repo 추가

---
+++ src/main/java/ssamchi/softeer/drivechat/repository/GuestRepository.java
@@ -0,0 +1,9 @@
+package ssamchi.softeer.drivechat.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ssamchi.softeer.drivechat.domain.Guest;
+
+public interface GuestRepository extends JpaRepository<Guest, Long> {
+    Optional<Guest> findByUser_UserId(Long userId);
+}
