50962a6f25007e9e33166a4fb1284b16a2a9129b
99winnmin <ojysep9987@gmail.com>
2024-01-06 11:59:08 +0900

    :sparkles: Feat : 운전자 없음 ERROR 코드 추가

--- src/main/java/ssamchi/softeer/drivechat/exception/Error.java
+++ src/main/java/ssamchi/softeer/drivechat/exception/Error.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 public enum Error {
     INTERNAL_SERVER_ERROR("서버 내부 에러입니다.", 500),
     // 사용자
-    USER_NOT_FOUND("사용자를 찾을 수 없습니다.", 1000)
-    ;
+    USER_NOT_FOUND("사용자를 찾을 수 없습니다.", 1000),
+    MARKER_NOT_FOUND("운전자를 찾을 수 없습니다.", 1001);
 
     private final String message;
     private final int errorCode;
