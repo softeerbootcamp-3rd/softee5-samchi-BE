e49bb757ce844d3a92731bf3f08d5a64b8acaa31
99winnmin <ojysep9987@gmail.com>
2024-01-08 15:22:24 +0900

    :zap: Feat : Review, Topic API 관련 swagger description 추가

--- src/main/java/ssamchi/softeer/drivechat/controller/TopicController.java
+++ src/main/java/ssamchi/softeer/drivechat/controller/TopicController.java
@@ -1,5 +1,7 @@
 package ssamchi.softeer.drivechat.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +15,14 @@ import ssamchi.softeer.drivechat.service.TopicService;
 
 import java.util.List;
 
+@Tag(name = "Topic 컨트롤러", description = "Topic 관련 API 목록입니다.")
 @RequestMapping("/api/topics")
 @RequiredArgsConstructor
 @RestController
 public class TopicController {
     private final TopicService topicService;
 
+    @Operation(summary = "전체 Topic Id 리스트 API", description = "서비스를 시작할 때 운전자/동승자 정보를 지정하는 과정에서 필요한 Topic Id 리스트 API 입니다.")
     @GetMapping
     public ResponseEntity<ResponseDto<List<AllTopicResponseDTO>>> getAllTopics() {
         List<AllTopicResponseDTO> topics = topicService.findAllTopics();
@@ -26,6 +30,7 @@ public class TopicController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Topic 랜덤 제시 API", description = "대화 주제를 랜덤으로 제시해주는 API 입니다.")
     @GetMapping("/content")
     public ResponseEntity<ResponseDto<TopicResponseDto>> getRandomContent(@RequestParam List<Long> ids) {
         TopicResponseDto topicResponseDto = topicService.getRandomContent(ids);
